818570a7773c680031d5bf3b5253b9bf766698fb
Zoltán Gubics <gubicsz@gmail.com>
2022-06-27 09:45:05 +0200

    Improved unit test readability with injection.

--- Assets/Tests/PlayerTests.cs
+++ Assets/Tests/PlayerTests.cs
@@ -13,41 +13,36 @@ public class PlayerTests : ZenjectUnitTestFixture
         Container.Bind<PlayerConfig>().AsSingle();
         Container.Bind<LevelConfig>().AsSingle();
         Container.Bind<PlayerModel>().AsSingle();
+        Container.Inject(this);
     }
+
+    [Inject] PlayerModel _player;
+    [Inject] PlayerConfig _config;
+    [Inject] LevelConfig _level;
 
     [Test]
     public void PlayerShouldStartAtSpawnPosition()
     {
-        var player = Container.Resolve<PlayerModel>();
-        var config = Container.Resolve<PlayerConfig>();
-
-        Assert.That(player.Position.Value == config.SpawnPosition);
+        Assert.That(_player.Position.Value == _config.SpawnPosition);
     }
 
     [Test]
     public void PlayerShouldBeAliveAtStart()
     {
-        var player = Container.Resolve<PlayerModel>();
-        var config = Container.Resolve<PlayerConfig>();
-
-        Assert.That(player.Lives.Value == config.Lives);
-        Assert.That(!player.IsDead.Value);
+        Assert.That(_player.Lives.Value == _config.Lives);
+        Assert.That(!_player.IsDead.Value);
     }
 
     [Test]
     public void PlayerShouldBeVulnerableAtStart()
     {
-        var player = Container.Resolve<PlayerModel>();
-
-        Assert.That(!player.IsInvulnerable.Value);
+        Assert.That(!_player.IsInvulnerable.Value);
     }
 
     [Test]
     public async void PlayerShouldBeDamagableAtStart()
     {
-        var player = Container.Resolve<PlayerModel>();
-
-        bool isDamaged = await player.DamageAsync();
+        bool isDamaged = await _player.DamageAsync();
 
         Assert.That(isDamaged);
     }
@@ -55,10 +50,9 @@ public class PlayerTests : ZenjectUnitTestFixture
     [Test]
     public async void PlayerShouldNotBeDamagableWhenDead()
     {
-        var player = Container.Resolve<PlayerModel>();
-        player.Lives.Value = 0;
+        _player.Lives.Value = 0;
 
-        bool isDamaged = await player.DamageAsync();
+        bool isDamaged = await _player.DamageAsync();
 
         Assert.That(!isDamaged);
     }
@@ -66,10 +60,9 @@ public class PlayerTests : ZenjectUnitTestFixture
     [Test]
     public async void PlayerShouldNotBeDamagableWhenInvulnerable()
     {
-        var player = Container.Resolve<PlayerModel>();
-        player.IsInvulnerable.Value = true;
+        _player.IsInvulnerable.Value = true;
 
-        bool isDamaged = await player.DamageAsync();
+        bool isDamaged = await _player.DamageAsync();
 
         Assert.That(!isDamaged);
     }
@@ -77,111 +70,96 @@ public class PlayerTests : ZenjectUnitTestFixture
     [Test]
     public void PlayerShouldLoseALifeWhenHit()
     {
-        var player = Container.Resolve<PlayerModel>();
-        int lives = player.Lives.Value;
-        player.DamageAsync().Forget();
+        int lives = _player.Lives.Value;
+        _player.DamageAsync().Forget();
 
-        Assert.That(player.Lives.Value == (lives - 1));
+        Assert.That(_player.Lives.Value == (lives - 1));
     }
 
     [Test]
     public void PlayerShouldBecomeInvulnerableWhenHit()
     {
-        var player = Container.Resolve<PlayerModel>();
-        player.DamageAsync().Forget();
+        _player.DamageAsync().Forget();
 
-        Assert.That(player.IsInvulnerable.Value);
+        Assert.That(_player.IsInvulnerable.Value);
     }
 
     [Test]
     public async void PlayerShouldBecomeVulnerableAfterBeingDamaged()
     {
-        var player = Container.Resolve<PlayerModel>();
-        await player.DamageAsync();
+        await _player.DamageAsync();
 
-        Assert.That(!player.IsInvulnerable.Value);
+        Assert.That(!_player.IsInvulnerable.Value);
     }
 
     [Test]
     public async void PlayerShouldBeInvulnerableForTheSpecifiedDuration()
     {
-        var player = Container.Resolve<PlayerModel>();
-        var config = Container.Resolve<PlayerConfig>();
-        player.DamageAsync().Forget();
+        _player.DamageAsync().Forget();
 
-        Assert.That(player.IsInvulnerable.Value);
+        Assert.That(_player.IsInvulnerable.Value);
 
-        await UniTask.Delay(System.TimeSpan.FromSeconds(config.Invulnerability));
+        await UniTask.Delay(System.TimeSpan.FromSeconds(_config.Invulnerability));
 
-        Assert.That(!player.IsInvulnerable.Value);
+        Assert.That(!_player.IsInvulnerable.Value);
     }
 
     [Test]
     public void PlayerShouldBeResetedProperly()
     {
-        var player = Container.Resolve<PlayerModel>();
-        var config = Container.Resolve<PlayerConfig>();
-        player.Position.Value = Vector3.right;
-        player.DamageAsync().Forget();
-        player.Shoot(10f);
+        _player.Position.Value = Vector3.right;
+        _player.DamageAsync().Forget();
+        _player.Shoot(10f);
 
-        player.Reset();
+        _player.Reset();
 
-        Assert.That(player.Position.Value == config.SpawnPosition);
-        Assert.That(player.Lives.Value == config.Lives);
-        Assert.That(!player.IsInvulnerable.Value);
-        Assert.That(player.ShotTime == float.MinValue);
+        Assert.That(_player.Position.Value == _config.SpawnPosition);
+        Assert.That(_player.Lives.Value == _config.Lives);
+        Assert.That(!_player.IsInvulnerable.Value);
+        Assert.That(_player.ShotTime == float.MinValue);
     }
 
     [Test]
     public void PlayerShouldMoveByTheExactAmount()
     {
-        var player = Container.Resolve<PlayerModel>();
-        var config = Container.Resolve<PlayerConfig>();
         float h = 1f;
         float dt = 1f / 60f;
-        float x = h * dt * config.Speed;
+        float x = h * dt * _config.Speed;
 
-        player.Move(h, dt);
+        _player.Move(h, dt);
 
-        Assert.That(player.Position.Value.x == x);
+        Assert.That(_player.Position.Value.x == x);
     }
 
     [Test]
     public void PlayerShouldNotMoveOutOfBounds()
     {
-        var player = Container.Resolve<PlayerModel>();
-        var level = Container.Resolve<LevelConfig>();
-
         float h = 1f;
         float dt = 1f / 60f;
-        player.Position.Value = new Vector3(level.Bounds.x, 
-            player.Position.Value.y, player.Position.Value.z);
+        _player.Position.Value = new Vector3(_level.Bounds.x, 
+            _player.Position.Value.y, _player.Position.Value.z);
 
-        player.Move(h, dt);
+        _player.Move(h, dt);
 
-        Assert.That(player.Position.Value.x == level.Bounds.x);
+        Assert.That(_player.Position.Value.x == _level.Bounds.x);
     }
 
     [Test]
     public void PlayerShouldBeAbleToShootAtStart()
     {
-        var player = Container.Resolve<PlayerModel>();
         float currentTime = 0f;
 
-        bool shot = player.Shoot(currentTime);
+        bool shot = _player.Shoot(currentTime);
 
         Assert.That(shot);
-        Assert.That(player.ShotTime == currentTime);
+        Assert.That(_player.ShotTime == currentTime);
     }
 
     [Test]
     public void PlayerShouldNotBeAbleToShootTwiceAtTheSameTime()
     {
-        var player = Container.Resolve<PlayerModel>();
-
-        bool shot1 = player.Shoot(0f);
-        bool shot2 = player.Shoot(0f);
+        bool shot1 = _player.Shoot(0f);
+        bool shot2 = _player.Shoot(0f);
 
         Assert.That(shot1);
         Assert.That(!shot2);
@@ -190,11 +168,8 @@ public class PlayerTests : ZenjectUnitTestFixture
     [Test]
     public void PlayerShouldBeAbleToShootBasedOnFireRate()
     {
-        var player = Container.Resolve<PlayerModel>();
-        var config = Container.Resolve<PlayerConfig>();
-
-        bool shot1 = player.Shoot(0f);
-        bool shot2 = player.Shoot(config.FireRate);
+        bool shot1 = _player.Shoot(0f);
+        bool shot2 = _player.Shoot(_config.FireRate);
 
         Assert.That(shot1);
         Assert.That(shot2);
