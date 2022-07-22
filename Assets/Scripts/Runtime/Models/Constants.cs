281484c7b77f92b9671ee85231bc86fd1044020d
Zoltán Gubics <gubicsz@gmail.com>
2022-07-22 21:52:51 +0200

    Relocated scripts and tests.

---
+++ Assets/Scripts/Runtime/Models/Constants.cs
@@ -0,0 +1,26 @@
+namespace SpaceInvaders
+{
+    public static class Constants
+    {
+        public class Objects
+        {
+            public const string Enemy = "Enemy";
+            public const string Player = "Player";
+            public const string Projectile = "Projectile";
+            public const string Blast = "Blast";
+        }
+
+        public class Audio
+        {
+            public const string Blaster = "Blaster";
+            public const string Explosion = "Explosion";
+            public const string Click = "Click";
+            public const string Music = "Music";
+        }
+
+        public class Materials
+        {
+            public const string Background = "Background";
+        }
+    }
+}
